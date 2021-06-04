7ba6711e08cf36f77e2818c273028cc5647f090b
GabrielFernandez14 <79731548+GabrielFernandez14@users.noreply.github.com>
2021-06-04 13:10:45 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
-/* Gabriel Fernandez
- * COP3330 - Summer CV01
- * "Motivated" Practice Exercises
- * Exercise 11 - Currency Conversion
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 Gabriel Fernandez
  */
 
 package org.example;
